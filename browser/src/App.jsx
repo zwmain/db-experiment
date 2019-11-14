a927cabc9495a9af6d52bd61e0034e8394270dc6
zwmain <409435808@qq.com>
2019-11-14 15:55:30 +0800

    项目配置

---
+++ browser/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(x) {
+    super(x);
+  }
+
+  render() {
+    return <div>数据库实验</div>;
+  }
+}
+
+export default App;
