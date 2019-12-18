69e42437b38bb08cd3a85a96a4a5f1b70bcfa5f0
zwmain <409435808@qq.com>
2019-12-18 21:12:40 +0800

    注释

--- server/src/server/login.js
+++ server/src/server/login.js
@@ -2,8 +2,10 @@ const Message = require('./Message');
 const { queryUser } = require('../api/user');
 
 function login(res, data) {
+  //根据前端传送过来的信息查询数据库
   queryUser(data)
     .then(results => {
+      //如果查询结果不为空则登录成功
       if (results.length > 0) {
         res.write(JSON.stringify(new Message(0, '登录成功', results[0])));
       } else {
@@ -11,9 +13,11 @@ function login(res, data) {
       }
     })
     .catch(e => {
+      //处理异常
       console.log(e);
     })
     .finally(() => {
+      //返回到前端
       res.end();
     });
 }
--- server/src/server/logon.js
+++ server/src/server/logon.js
@@ -2,11 +2,15 @@ const Message = require('./Message');
 const {queryUserById, insertUser} = require('../api/user');
 
 async function logon(res, data) {
+    //查询用户是否已经注册
     let users = await queryUserById(data.userId);
     if (users.length > 0) {
+        //若已经注册，注册失败
         res.write(JSON.stringify(new Message(1, "用户已存在")));
     } else {
+        //添加到用户表
         let results = await insertUser(data);
+        //判断添加是否成功
         if (results !== null) {
             res.write(JSON.stringify(new Message(0, "注册成功", data)));
         } else {
